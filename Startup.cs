faa5dce7e35523d66e17798750447518afc29389
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-10-07 19:57:49 -0400

    Añadiendo services.AddBlazoredToast(); a la clase Startup y agregando los using en imports.razor

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using LuisDavidGil_P1_AP2.Data;
+using Blazored.Toast;
 
 namespace LuisDavidGil_P1_AP2
 {
@@ -30,6 +31,7 @@ namespace LuisDavidGil_P1_AP2
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
