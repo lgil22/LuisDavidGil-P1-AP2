5cbe91dcfa6518388eaf4d78c11d03711cf15172
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-10-07 20:58:51 -0400

    Añadiendo migración

---
+++ Migrations/20201008005650_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LuisDavidGil_P1_AP2.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(maxLength: 200, nullable: false),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<decimal>(nullable: false),
+                    ValorInventario = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
